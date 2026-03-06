1c596ccccc80ded8a58ede951d5df82248cd0e68
DominicDev <s0l0estudi4r@gmail.com>
2026-03-06 16:37:40 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PANTERA';
+export let nameSongs = [
+	"WALK ", /*1*/
+	"CEMETERY GATES", /*2*/
+	"COWBOYS FROM HELL", /*3*/
+	"DOMINATION", /*4*/
+	"THIS LOVE", /*5*/
+	"MOUTH FOR WAR", /*6*/
+	"I'M BROKEN", /*7*/
+	"FUCKING HOSTILE", /*8*/
+	"A NEW LEVEL", /*9*/
+	"5 MINUTES ALONE", /*10*/
+	"BECOMING", /*11*/
+	"STRENGTH BEYOND STRENGTH", /*12*/
+	"PRIMAL CONCRETE SLEDGE", /*13*/
+	"REVOLUTION IS MY NAME", /*14*/
+	"GRAVEYARD DISCIPLES", /*15*/
+	"SHATTERED", /*16*/
+	"BY DEMONS BE DRIVEN", /*17*/
+	"HOLLOW", /*18*/
+	"RIOT", /*19*/
+	"FLOODS", /*20*/
+]
